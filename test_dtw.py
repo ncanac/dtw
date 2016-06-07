838a47ff322ea834e05cb98ed90d29629d95eeaa
Nicolas Canac <nicolas@neuralanalytics.com>
2016-06-07 15:07:33 -0700

    Made all command line arguments optional

--- test_dtw.py
+++ test_dtw.py
@@ -3,10 +3,12 @@ from _dtw import dtw
 import sys
 import time
 
-size = int(sys.argv[1])
-window = 1.0
-if len(sys.argv) > 2:
-    window = float(sys.argv[2])
+size = 10000
+window = 0.1
+if len(sys.argv) > 1:
+    size = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        window = float(sys.argv[2])
 
 x = rand(size)
 y = rand(size)
